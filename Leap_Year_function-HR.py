697d9e0bc65c4e975d06e8e331b8975c3852a945
Alex Stanuta <alex.stanuta@gmail.com>
2020-06-16 17:23:11 +0300

    Add the solution to the https://www.hackerrank.com/challenges/write-a-function/problem

---
+++ Leap_Year_function-HR.py
@@ -0,0 +1,16 @@
+#You are given the year, and you have to write a function to check if the year is leap or not.
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0 and year % 100 == 0:
+        if year % 400 == 0:
+            leap = True
+    elif year % 4 == 0:
+        leap = True
+    
+    return leap
+
+if __name__ == "__main__":
+	year = int(input())
+	print(is_leap(year))
